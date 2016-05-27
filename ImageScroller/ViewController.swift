dcc89dfbb4c36538dec5b33a18d77b6713ee6142
Bill Yu <han.yu@duke.edu>
2016-05-26 22:24:01 -0400

    finish project

--- ImageScroller/ViewController.swift
+++ ImageScroller/ViewController.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var imageView: UIImageView!
+    var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView = UIImageView(image: UIImage(named: "zombies.jpg"))
+        scrollView = UIScrollView(frame: view.bounds)
+        scrollView.autoresizingMask = [.FlexibleWidth,.FlexibleHeight]
+        scrollView.backgroundColor = UIColor.blackColor()
+        scrollView.contentSize = imageView.bounds.size
+        
+        scrollView.addSubview(imageView)
+        view.addSubview(scrollView)
+        
+        scrollView.delegate = self
+        setZoomParametersForSize(scrollView.bounds.size)
+        
+        recenterImage()
+    }
+    
+    func setZoomParametersForSize(scrollViewSize: CGSize) {
+        let imageSize = imageView.bounds.size
+        let widthScale = scrollViewSize.width / imageSize.width
+        let heightScale = scrollViewSize.height / imageSize.height
+        let minScale = min(widthScale, heightScale)
+        
+        scrollView.minimumZoomScale = minScale
+        scrollView.maximumZoomScale = 3.0
+        scrollView.zoomScale = minScale
+    }
+    
+    func recenterImage() {
+        let scrollViewSize = scrollView.bounds.size
+        let imageSize = imageView.frame.size
+        
+        let horizontalSpace = imageSize.width < scrollViewSize.width ?
+            (scrollViewSize.width - imageSize.width) / 2 : 0
+        let verticalSpace = imageSize.height < scrollViewSize.height ?
+            (scrollViewSize.height - imageSize.height) / 2 : 0
+        scrollView.contentInset = UIEdgeInsets(top: verticalSpace, left: horizontalSpace, bottom: verticalSpace, right: horizontalSpace)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        setZoomParametersForSize(scrollView.bounds.size)
+        recenterImage()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: UIScrollViewDelegate {
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        recenterImage()
+    }
+}
